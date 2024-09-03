b398e01e7e301c25b2df66bd7d9997d2d9b51bdc
Coaixy <Coaixy@outlook.com>
2024-09-03 15:36:40 +0800

    add new info in header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,24 @@
 import {Component} from "solid-js";
 import styles from '../Styles/App.module.css';
-const Header:Component = ()=>{
+import {ALL_DONE_COUNT, ALL_ERROR_COUNT, ALL_QUESTION_COUNT} from "../utils";
+
+const Header: Component = () => {
     return (
-        <div class={styles.header}>
+        <div className={styles.header}>
             Review-Tools Beta
+
+            <div className={styles.subHeader}>
+                <span>成功解析题目数:</span>
+                <span>{ALL_QUESTION_COUNT()}</span>
+            </div>
+            <div className={styles.subHeader}>
+                <span>已复习题目数:</span>
+                <span>{ALL_DONE_COUNT()}</span>
+            </div>
+            <div className={styles.subHeader}>
+                <span>已复习题目错题数:</span>
+                <span>{ALL_ERROR_COUNT()}</span>
+            </div>
         </div>
     )
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,12 @@
+import {createSignal} from "solid-js";
+
 export function getMiddleStr(fullStr: string, prefix: string, suffix: string) {
     const start = prefix.length;
     const end = fullStr.length - suffix.length;
     // 返回中间文本
     return fullStr.slice(start, end);
 }
+
+export const [ALL_QUESTION_COUNT, set_ALL_QUESTION_COUNT] = createSignal(0)
+export const [ALL_DONE_COUNT, set_ALL_DONE_COUNT] = createSignal(0)
+export const [ALL_ERROR_COUNT, set_ALL_ERROR_COUNT] = createSignal(0)
